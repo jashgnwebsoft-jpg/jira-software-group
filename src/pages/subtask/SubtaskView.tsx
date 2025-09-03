8bd10f890e560d71499bfbda32d1b0cfa548bc88
himanshupansuriya-gnwebsoft <himanshupansuriya.gnwebsoft@gmail.com>
2025-09-03 17:25:04 +0530

    name changed in subtask vieww

--- src/pages/subtask/SubtaskView.tsx
+++ src/pages/subtask/SubtaskView.tsx
@@ -338,7 +338,7 @@ export default function SubtaskView() {
                     alignItems="center"
                   >
                     <Avatar
-                      alt="Jash Tank"
+                      alt="Himanshu Pansuriya"
                       src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                     />
                     <Typography variant="body1">Jash Tank</Typography>
