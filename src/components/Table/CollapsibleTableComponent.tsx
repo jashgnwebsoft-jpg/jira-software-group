8e8b50226faf776fc64db1d9d9a1029ee60b678f
Deep Bala <deep.gnwebsoft@gmail.com>
2025-09-04 10:13:11 +0530

    change modal open

--- src/components/Table/CollapsibleTableComponent.tsx
+++ src/components/Table/CollapsibleTableComponent.tsx
@@ -165,23 +165,22 @@ function Row(props: { row: Goal }) {
         <TableCell>
           <IconButton onClick={handleModalOpen}>
             <MoreHoriz />
-            <Modal
-              open={modalopen}
-              onClose={handleClose}
-              aria-labelledby="modal-modal-title"
-              aria-describedby="modal-modal-description"
-            >
-              <Box sx={style}>
-                <Typography id="modal-modal-title" variant="h6" component="h2">
-                  Text in a modal
-                </Typography>
-                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                  Duis mollis, est non commodo luctus, nisi erat porttitor
-                  ligula.
-                </Typography>
-              </Box>
-            </Modal>
           </IconButton>
+          <Modal
+            open={modalopen}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                Text in a modal
+              </Typography>
+              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+              </Typography>
+            </Box>
+          </Modal>
         </TableCell>
       </TableRow>
       <TableRow>
